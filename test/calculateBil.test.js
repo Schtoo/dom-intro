9e487035521774d2bdae41148713e94770ebb743
Schtoo <mgqobhoz@gmail.com>
2018-05-22 12:02:49 +0200

    added more scenarios for calculateBill widget

--- test/calculateBil.test.js
+++ test/calculateBil.test.js
@@ -5,7 +5,13 @@ describe('calculate bill', function() {
   it("should give you the amount of an sms enetered", function() {
     assert.equal(calculateBtnClicked('sms'), 0.75);
   });
-  it('should give you the total for both sms & call', function(){
-    assert.equal(calculateBtnClicked('sms, call, sms, call'),7.00);
+  it('should give you the total for both sms & call', function() {
+    assert.equal(calculateBtnClicked('sms, call, sms, call'), 7.00);
+  });
+  it('should give you an error if entered a number', function() {
+    assert.equal(calculateBtnClicked('20'), 0.00);
+  });
+  it('should return nothing if nothing is entered', function() {
+    assert.equal(calculateBtnClicked(''), 0.00);
   })
-})
+});
