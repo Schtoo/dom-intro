daa9e6d4c1d80b2fe23301cb873a22ed65d5eb4a
Schtoo <mgqobhoz@gmail.com>
2018-05-22 12:08:30 +0200

    added calculateBill widget

--- test/calculateBil.test.js
+++ test/calculateBil.test.js
@@ -9,9 +9,9 @@ describe('calculate bill', function() {
     assert.equal(calculateBtnClicked('sms, call, sms, call'), 7.00);
   });
   it('should give you an error if entered a number', function() {
-    assert.equal(calculateBtnClicked('20'), 0.00);
+    assert.equal(calculateBtnClicked('20'), 0.);
   });
   it('should return nothing if nothing is entered', function() {
-    assert.equal(calculateBtnClicked(''), 0.00);
+    assert.equal(calculateBtnClicked(''), 0.);
   })
 });
